4f11a49c75c91aa50e5f2aaeb2468484ed0998a9
duongnlhs150319 <duongnlhs150319@gmail.com>
2025-05-15 20:09:18 +0700

    them ngoac

--- Program.cs
+++ Program.cs
@@ -9,12 +9,20 @@
         double bmi = weight / Math.Pow(height, 2); ;
         Console.WriteLine("BMI = " + bmi);
         if (bmi < 18.5)
+        {
             Console.WriteLine("Underweight");
-        else if (bmi >= 18.5 && bmi < 25.0)
+        }
+        else if ((bmi >= 18.5) && (bmi < 25.0))
+        {
             Console.WriteLine("Normal");
-        else if (bmi >= 25.00 && bmi < 30.0)
+        }
+        else if ((bmi >= 25.00) && (bmi < 30.0))
+        {
             Console.WriteLine("Overweight");
+        }
         else
-            Console.WriteLine("Obese");    
+        {
+            Console.WriteLine("Obese");
+        }  
     }   
 }
